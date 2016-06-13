a1ac6e4b3903df6dd86e15be4886df6ca1b2b4c8
Mehdi Karamosly <mehdi.karamosly@breaktech.com>
2016-06-13 14:09:11 -0500

    Checking Unit test failure.

--- tests/BasicTest.php
+++ tests/BasicTest.php
@@ -4,7 +4,7 @@ require_once(dirname(__FILE__) . '/../vendor/autoload.php');
 class SimpleTestCase extends TestCase {
 
 	function testSimpleScenario(){
-		$this->assertTrue(true);
+		$this->assertTrue(false);
 
 	}
 
